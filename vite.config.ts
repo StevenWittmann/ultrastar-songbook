ca1ff1e37eb4f1db8d4722c339c2120ff206044c
Steven Herrmann <s.herrmann@dotsource.de>
2023-09-10 18:40:49 +0200

    vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: false,
+			},
+		}),
+	],
 	server: {
 		watch: {
 			usePolling: true,
